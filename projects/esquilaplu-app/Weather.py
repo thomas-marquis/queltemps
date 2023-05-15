0ab4e876b647790d3fae3b4b2e084ecf9cfe4483
Thomas Marquis <thomas.marquis.dev@gmail.com>
2023-05-16 00:30:23 +0200

    use S3 to store data

--- projects/esquilaplu-app/Weather.py
+++ projects/esquilaplu-app/Weather.py
@@ -3,11 +3,40 @@ import pandas as pd
 import datetime as dt
 from src.utils import render_hide_st_burger_menu
 import os
+import boto3
+from dotenv import load_dotenv
+
+load_dotenv()
 
 st.set_page_config(page_title="Esquilaplu", page_icon="🌞")
 render_hide_st_burger_menu()
 
 STATION_ID = 7510
+
+
+
+class WeatherRepository:
+    def __init__(self) -> None:
+        self._aws_s3_bucket = os.getenv("S3_BUCKET")
+        self._aws_access_key_id = os.getenv("ACCESS_KEY_ID")
+        self._aws_secret_access_key = os.getenv("SECRET_ACCESS_KEY")
+
+        self._root_key = "esquilaplu"
+        
+        self._s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=self._aws_access_key_id,
+            aws_secret_access_key=self._aws_secret_access_key,
+        )
+        
+    def load_dataset(self, dataset_id: str) -> pd.DataFrame:
+        data_key = f"{self._root_key}/{dataset_id}.csv"
+        data_object = self._s3_client.get_object(Bucket=self._aws_s3_bucket, Key=data_key)
+        data = pd.read_csv(data_object["Body"], sep=";", header=0, parse_dates=["date"])
+        
+        return data
+
+
 
 
 class WeatherChunk:
@@ -54,9 +83,10 @@ class WeatherChunk:
 class WeatherChunkFactory:
     def __init__(self, data_dir_path: str) -> None:
         self._data_dir_path = data_dir_path
+        self._repository = WeatherRepository()
 
     def get_chunk_by_date_and_time(self, datetime: dt.datetime) -> WeatherChunk:
-        data = pd.read_csv(f"{self._data_dir_path}/{datetime.date().isoformat()}-{datetime.hour}.csv", sep=";", header=0, parse_dates=["date"])
+        data = self._repository.load_dataset(f"{datetime.date().isoformat()}-{datetime.hour}")
         return WeatherChunk(data, datetime)
     
     def get_chunks_by_date(self, date: dt.date) -> list[WeatherChunk]:
